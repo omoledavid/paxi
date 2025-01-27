c5f72f8e46ef537d2a723546a73436c879ab1f38
Omole kessiena David <edokadavid@gmail.com>
2025-01-27 12:07:34 +0100

    made changes to auth

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -4,14 +4,11 @@ namespace App\Http\Controllers\Api\Auth;
 
 
 use App\Http\Controllers\Controller;
-use App\Mail\SendVerificationCode;
 use App\Models\User;
 use App\Traits\ApiResponses;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -61,7 +58,6 @@ class AuthController extends Controller
             'token' => $token
         ]);
     }
-    use Illuminate\Support\Facades\Hash;
 
     public function login(Request $request)
     {
