aadbfd87c1ea0a7099560979c590d079a26b0a86
Omole David <omolekessiena@gmail.com>
2026-03-07 18:35:53 +0100

    feat: add AuthorizationController for user authorization, email, and SMS verification flows.

--- app/Http/Controllers/Api/Auth/AuthorizationController.php
+++ app/Http/Controllers/Api/Auth/AuthorizationController.php
@@ -96,10 +96,11 @@ class AuthorizationController extends Controller
 
         // Save code and expiry to database
         $user->sMobileVerCode = $code;
-        $user->sMobileVerCodeExpiry = Carbon::now()->addMinutes(1);
+        $user->sMobileVerCodeExpiry = Carbon::now()->addMinutes(5);
         $user->updated_at = Carbon::now();
         $user->save();
-
+        // refresh user
+        $user->refresh();
         // Send SMS via GatewayAPI
         $smsSent = sendSmsVerificationCode($code, $user->sPhone);
 
